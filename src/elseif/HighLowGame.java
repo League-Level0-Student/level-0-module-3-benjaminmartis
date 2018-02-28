40de597d03f3e656c0b4c322d9585f62b583d47b
Martis.Ben <Martis.Ben@SEMS-259-36.SEMS259.LAB>
2018-02-28 14:58:32 -0800

    high low game

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -11,20 +11,31 @@ public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random(1).nextInt(100);
+		int random = new Random().nextInt(100);
 		// 2. Print out the random variable above
 		System.out.println(random);
 		// 11. do the following 10 times
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-		JOptionPane.showInputDialog("what number is your geuss?");
+		String a = JOptionPane.showInputDialog("what number is your guess?");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int b=Integer.parseInt(a);
 			// 5. if the guess is correct
+			if (b==random) {
+				JOptionPane.showMessageDialog(null, "WIN!");
+			}
 				// 6. win
 			// 7. if the guess is high
+			if ( b>random ) {
+				JOptionPane.showMessageDialog(null, "Sorry but your geuss is too high");
+				JOptionPane.showMessageDialog(null, "YOU LOSE!");
+			}
 				// 8. tell them it's too high
 			// 9. if the guess is low
+			if (b<random) {
+				JOptionPane.showMessageDialog(null, "sorry but your geuss is too low");
+				JOptionPane.showMessageDialog(null, "YOU LOSE!");
+			}
 				// 10. tell them it's too low
 
 		// 12. tell them they lose
